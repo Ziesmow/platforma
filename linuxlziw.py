3eee3db8193fc8b7062fbbd6c7154911cdc4c288
Ziesmow <Ziesmow@proton.me>
2025-01-21 19:46:34 +0100

    wydarzenia lepjej działają, port linuxexclusive, miesiąc dodany random things work better, import linux exclusive, month added

---
+++ linuxlziw.py
@@ -0,0 +1,13 @@
+
+def linuksexclusiv():
+    print("88     88 88b 88 88   88 Yb  dP ")
+    print("88     88 88Yb88 88   88  YbdP  ")
+    print("88  .o 88 88 Y88 Y8   8P  dPYb  ")
+    print("88ood8 88 88  Y8 `YbodP' dP  Yb ")
+    print()
+    print("888888 Yb  dP  dP88b8 88     88   88 .dPY8  88 Yb    dP 888888 ")
+    print("88__    YbdP  dP      88     88   88 `Ybo.  88  Yb  dP  88__   ")
+    print("88      dPYb  Yb      88  .o Y8   8P o.`Y8b 88   YbdP   88""   ")
+    print("888888 dP  Yb  YboodP 88ood8 `YbodP' 8bodP' 88    YP    888888 ")
+
+
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import random
+import linuxlziw
+day = 1
 
-
+def monthend(day):
+    if day == 31 :
+        day = 1 
+        print("zaczyna się nowy miesiac")
+    else :
+        print(f"obecny dzien to {day} -y miesiąca")
+        
 
 def kompas():
     Strony = ["północ", "zachód", "wschód", "południe"]
@@ -9,18 +17,23 @@ def wiatr(NWES,):
     print(f"nadchodzi wiatr z strony {NWES}")
 def sztorm(NWES,):
     print(f"nadchodzi sztorm z {NWES} tego dnia lodzie nie mogą przypływać oraz odpływwać z platformy")
-listaf = [ "wiatr", "sztorm"]
+def deszcz(NWES):
+    print(f"deszcz nadchodzi ze strony {NWES}")
+listaf = [ "wiatr", "sztorm", "deszcz"]
 
-class surowiec():
-    
-    def __init__(self)
-
-def wydarzenia(NWES):
+def wydarzenia():
+    monthend(day)
     NWES = kompas()
     wydarzenie = random.choice(listaf)
     if wydarzenie == "wiatr" :
         wiatr(NWES)
     elif wydarzenie == "sztorm" :
         sztorm(NWES)
+    elif wydarzenie == "deszcz" :
+        deszcz(NWES)
+
+linuxlziw.linuksexclusiv()
+
+wydarzenia()
 
 
